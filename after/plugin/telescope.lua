6a9f38fb120e414d07aeb4dfded1cddfa8dcfb43
Igor Racki <igor.racki@outlook.com>
2024-02-02 10:59:16 +0000

    Update telescope.lua

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -2,7 +2,6 @@ local builtin = require('telescope.builtin')
 --vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.git_files, {})
-vim.keymap.set('n', '<C-s>', function()
-	builtin.grep_string({ search = vim.fn.input("Grep > " ) });
-end)
+-- requires ripgrep installed on the system
+vim.keymap.set('n', '<C-s>', builtin.live_grep, {})
 
